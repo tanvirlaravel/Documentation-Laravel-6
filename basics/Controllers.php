5223dcc43f95be1ce4d0e4bcdbbd9d28a8645e85
tanvir <tanvirlaravel@gmail.com>
2021-08-10 23:31:47 +0600

    controller

--- basics/Controllers.php
+++ basics/Controllers.php
@@ -12,23 +12,26 @@
 
             <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
 
-                <h4 class="mb-4 py-2 pl-5 pr-2 bg-info d-inline-block">Introduction</h4>
+                <h2 class="mb-4 py-2 pl-5 pr-2 bg-info d-inline-block">Introduction</h2>
                 <p>Nothing</p>
 
             </div>
 
             <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
-                <h4 class="mb-4 py-2 pl-5 pr-2 bg-info d-inline-block">Basic Controllers</h4>
+                <h2 class="mb-4 py-2 pl-5 pr-2 bg-info d-inline-block">Basic Controllers</h2>
+                <h3>Defining Controllers</h3>
+                <h3>Controllers & Namespaces</h3>
+                <h3>Single Action Controllers</h3>
                
-
+                <hr>
                 <h4>Defining Controllers</h4>
-                <p>the controller extends the base controller class included with Laravel. The base class provides a few convenience methods such as the middleware method
+                <p>The base class provides a few convenience methods such as the <code>middleware</code> method
                 </p>
                 <p><code>Route::get('user/{id}', 'UserController@show');</code></p>
                 <p>when a request matches the specified route URI, the <code>show</code> method on the <code>UserController</code> class will be executed. The route parameters will also be passed to the method.</p>
                 <hr>
 
-                <h4>Controllers & Namespaces <span class="text-danger">later</span></h4>
+                <h4>Controllers & Namespaces</h4>
                 <a href="https://laravel.com/docs/6.x/controllers#controllers-and-namespaces" target="_blank">Controllers & Namespaces</a>
                 <hr>
                 
@@ -39,48 +42,60 @@
             </div>
 
             <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
-                <h4 class="mt-5 mb-4 py-2 pl-5 pr-2 bg-info d-inline-block">Controller Middleware</h4>
+                <h2 class="mt-5 mb-4 py-2 pl-5 pr-2 bg-info d-inline-block">Controller Middleware</h2>
+                <hr>
+                <code>Route::get('profile', 'UserController@show')->middleware('auth');</code>
                 <pre class="">
-                class UserController extends Controller
-                {   
-                    public function __construct()
-                    {
-                        $this->middleware('auth');
+    class UserController extends Controller
+    {   
+        public function __construct()
+        {
+            $this->middleware('auth');
 
-                        $this->middleware('log')->only('index');
+            $this->middleware('log')->only('index');
 
-                        $this->middleware('subscribed')->except('store');
-                    }
-                }
+            $this->middleware('subscribed')->except('store');
+        }
+    }
                 </pre>
 
                 <p>Controllers also allow you to register middleware using a Closure. This provides a convenient way to define a middleware for a single controller without defining an entire middleware class:</p>
                 <pre>
-                $this->middleware(function ($request, $next) {
-                    // ...
+    $this->middleware(function ($request, $next) {
+        // ...
 
-                    return $next($request);
-                });
+        return $next($request);
+    });
                 </pre>
             </div>
 
             <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
-                <h4 class="mb-4 py-2 pl-5 pr-2 bg-info d-inline-block">Resource Controllers</h4>
-                <pre>
-                    Route::resource('photos', 'PhotoController');
+                <h2 class="mb-4 py-2 pl-5 pr-2 bg-info d-inline-block">Resource Controllers</h2>
+                <h3>Partial Resource Routes</h3>
+                <h3>Nested Resources</h3>
+                <h3>Naming Resource Route Parameters</h3>
+                <h3>Localizing Resource URIs</h3>
+                <h3>Supplementing Resource Controllers</h3>
 
-                    Route::resources([
-                        'photos' => 'PhotoController',
-                        'posts' => 'PostController'
-                    ]);
+<hr>
+
+                <pre>
+    Route::resource('photos', 'PhotoController');
+
+    Route::resources([
+        'photos' => 'PhotoController',
+        'posts' => 'PostController'
+    ]);
                 </pre>
 
                 <h4>Spoofing Form Methods</h4>
                 <p>PUT, PATCH, or DELETE requests</p>
                 <pre>
-                form action="/foo/bar" method="POST">
-                    @method('PUT')
-                /form>
+    form action="/foo/bar" method="POST">
+        @method('PUT')
+        @method('PATCH')
+        @method('DELETE')
+    /form>
                 </pre>
 
                 <h4>Partial Resource Routes</h4>
@@ -112,23 +127,23 @@
                 <h4 class="mt-5 mb-4 py-2 pl-5 pr-2 bg-info d-inline-block"> Dependency Injection & Controllers</h4>
                 <p>The Laravel service container is used to resolve all Laravel controllers. As a result, you are able to type-hint any dependencies your controller may need in its constructor. The declared dependencies will automatically be resolved and injected into the controller instance:</p>
                 <pre>
-                protected $users;
+    protected $users;
 
-                public function __construct(UserRepository $users)
-                {
-                    $this->users = $users;
-                }
+    public function __construct(UserRepository $users)
+    {
+        $this->users = $users;
+    }
                 </pre>
 
                 <hr>
                 <h4>Method Injection</h4>
                 <pre>
-                public function update(Request $request, $id)
-                {
-                    $name = $request->name;
+    public function update(Request $request, $id)
+    {
+        $name = $request->name;
 
-                    //
-                }
+        //
+    }
                 </pre>
 
 
